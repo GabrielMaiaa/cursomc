39a21cf99c81cebd8563557dfecdf5418223d6bb
Gabriel Baptistella Maia <gabrielbmaiaa@gmail.com>
2021-10-21 14:14:28 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -8,57 +8,55 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity 
-public class Categoria implements Serializable {	
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Integer id;
-		private String nome;
-		
-		public Categoria() {		
-		}
 
-		public Categoria(Integer id, String nome) {
-			super();
-			this.id = id;
-			this.nome = nome;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
 
-		public Integer getId() {
-			return id;
-		}
+	public Categoria() {
+	}
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
 
-		public String getNome() {
-			return nome;
-		}
+	public Integer getId() {
+		return id;
+	}
 
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(id);
-		}
+	public String getNome() {
+		return nome;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Categoria other = (Categoria) obj;
-			return Objects.equals(id, other.id);
-		}
-	
-		
-		
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
 }
